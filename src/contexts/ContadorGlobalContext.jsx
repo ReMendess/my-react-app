39739a2a3995a90132d567370e91897ec76833d2
Renan <renan_oliveira_99@hotmail.com>
2026-03-14 15:18:07 -0300

    V3

---
+++ src/contexts/ContadorGlobalContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+
+export const ContadorContext = createContext();
+
+export function ContadorProvider({children}){
+    const [contador, setContador] = useState(0);
+
+    function incrementar(){
+        setContador(contador + 1);
+    }
+
+    function decrementar(){
+        setContador(contador - 1);
+    }
+
+    return(
+        <ContadorContext.Provider value = {{contador, incrementar, decrementar}}>
+            {children}
+        </ContadorContext.Provider>
+    );
+
+}
